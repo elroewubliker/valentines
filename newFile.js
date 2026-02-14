c70392706dfeb5ddeb9e1de8e44f45e3e8f6afcb
elroewubliker <elroe.wubliker@aastustudent.edu.et>
2026-02-14 22:04:12 +0300

    Add files via upload

---
+++ newFile.js
@@ -0,0 +1,11 @@
+const { envelope, letter } = require("./script");
+
+// Click envelope
+envelope.addEventListener("click", () => {
+    envelope.style.display = "none";
+    letter.style.display = "flex";
+
+    setTimeout(() => {
+        document.querySelector(".letter-window").classList.add("open");
+    }, 50);
+});
